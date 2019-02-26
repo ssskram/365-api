7faa0470af235e32cf92f67020ba076d063aff9c
ssskram <paul.marks@pittsburghpa.gov>
2019-02-26 10:52:43 -0500

    pgh certs endpoints

--- models/pghCerts.js
+++ models/pghCerts.js
@@ -1,27 +1,35 @@
+const userProfiles = {
+    list: 'value',
+    item: {
+        id: 'Id',
+        email: 'Email',
+        department: 'Department',
+        title: 'Title'
+    }
+}
+
 const certs = {
     list: 'value',
     item: {
-        certName: 'CertName',
-        certNumber: 'CertNumber'
+        certID: 'Certification_x0020_ID',
+        certName: 'Certification',
+        ICC: 'ICC',
+        UCC: 'UCC'
     }
 }
 
 const certHistory = {
     list: 'value',
     item: {
-
-    }
-}
-
-const courseHistory = {
-    list: 'value',
-    item: {
-
+        entryId: 'Id',
+        user: 'User',
+        certId: 'Certification_x0020_ID',
+        date: 'Date'
     }
 }
 
 module.exports = {
+    userProfiles,
     certs,
-    certHistory,
-    courseHistory
+    certHistory
 }
--- routes/pghCerts.js
+++ routes/pghCerts.js
@@ -8,6 +8,81 @@ const models = require('../models/pghCerts')
 
 global.Headers = fetch.Headers
 
+// returns user profile, if existent
+router.get('/userProfile',
+    async function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            fetch("https://cityofpittsburgh.sharepoint.com/sites/certs/_api/web/lists/GetByTitle('User Profiles')/items?$filter=Email eq '" + req.query.user + "'", {
+                    method: 'GET',
+                    headers: new Headers({
+                        'Authorization': 'Bearer ' + await refreshToken(),
+                        'Accept': 'application/json'
+                    })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    res.status(200).send(dt(data, models.userProfiles).transform())
+                })
+                .catch(err => console.log(err))
+        } else res.status(403).end()
+    }
+)
+
+// creates new user profile, returns the id
+router.post('/userProfile',
+    async function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            await fetch("https://cityofpittsburgh.sharepoint.com/sites/certs/_api/web/lists/GetByTitle('User Profiles')/items", {
+                    method: 'POST',
+                    headers: new Headers({
+                        'Authorization': 'Bearer ' + await refreshToken(),
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    }),
+                    body: JSON.stringify(req.body)
+                })
+                .then(res => res.json())
+                .then(data => {
+                    res.status(200).send({
+                        id: data.Id
+                    })
+                })
+                .catch(error => res.status(500).send(error))
+        } else res.status(403).end()
+    }
+)
+
+// returns true if supplied email param is contained in SP group
+router.get('/isAdmin',
+    async function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            fetch("https://cityofpittsburgh.sharepoint.com/sites/certs/_api/web/sitegroups(8)/users?$select=Email", {
+                    method: 'GET',
+                    headers: new Headers({
+                        'Authorization': 'Bearer ' + await refreshToken(),
+                        'Accept': 'application/json'
+                    })
+                })
+                .then(res => res.json())
+                .then(data => {
+                    if (data["value"].find(admin => admin.Email == req.query.user)) {
+                        res.status(200).send({
+                            isAdmin: true
+                        })
+                    } else {
+                        res.status(200).send({
+                            isAdmin: false
+                        })
+                    }
+                })
+                .catch(err => res.status(500).send(err))
+        } else res.status(403).end()
+    }
+)
+
 // get list of certifications
 router.get('/certTypes',
     async function (req, res) {
@@ -24,7 +99,7 @@ router.get('/certTypes',
                 .then(data => {
                     res.status(200).send(dt(data, models.certs).transform())
                 })
-                .catch(err => console.log(err))
+                .catch(err => res.status(500).send(err))
         } else res.status(403).end()
     }
 )
@@ -45,28 +120,47 @@ router.get('/certHistory',
                 .then(data => {
                     res.status(200).send(dt(data, models.certHistory).transform())
                 })
-                .catch(err => console.log(err))
+                .catch(err => res.status(500).send(err))
         } else res.status(403).end()
     }
 )
 
-// get all course history
-router.get('/courseHistory',
+// new cert record
+router.post('/certHistory',
     async function (req, res) {
         const valid = (checkToken(req.token))
         if (valid == true) {
-            fetch("https://cityofpittsburgh.sharepoint.com/sites/certs/_api/web/lists/GetByTitle('Course History')/items?$top=5000", {
-                    method: 'GET',
+            fetch("https://cityofpittsburgh.sharepoint.com/sites/certs/_api/web/lists/GetByTitle('Cert History')/items", {
+                    method: 'POST',
                     headers: new Headers({
                         'Authorization': 'Bearer ' + await refreshToken(),
                         'Accept': 'application/json'
-                    })
+                    }),
+                    body: JSON.stringify(req.body)
                 })
-                .then(res => res.json())
-                .then(data => {
-                    res.status(200).send(dt(data, models.courseHistory).transform())
+                .then(res.status(200).send())
+                .catch(error => res.status(500).send(error))
+        } else res.status(403).end()
+    }
+)
+
+// update cert record
+router.post('/updateCourse',
+    async function (req, res) {
+        const valid = (checkToken(req.token))
+        if (valid == true) {
+            fetch("https://cityofpittsburgh.sharepoint.com/sites/certs/_api/web/lists/GetByTitle('Cert History')/items(" + req.query.id + ")", {
+                    method: 'MERGE',
+                    headers: new Headers({
+                        'Authorization': 'Bearer ' + await refreshToken(),
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        "IF-MATCH": "*"
+                    }),
+                    body: JSON.stringify(req.body)
                 })
-                .catch(err => console.log(err))
+                .then(res.status(200).send())
+                .catch(error => res.status(500).send(error))
         } else res.status(403).end()
     }
 )
