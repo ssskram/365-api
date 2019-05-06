c699399b5bc474d95983579b1fcce471e58a1b50
ssskram <paul.marks@pittsburghpa.gov>
2019-05-06 14:09:09 -0400

    GET for performance  imnprovement survey responses

---
+++ models/pts.js
@@ -0,0 +1,15 @@
+const responses = {
+  list: "value",
+  item: {
+    user: "Human",
+    department: "Department",
+    submitted: "Created",
+    budgets: "Budgets",
+    processes: "Processes",
+    goalsAndTraining: "Goals_x0020_and_x0020_Training"
+  }
+};
+
+module.exports = {
+  responses
+};
