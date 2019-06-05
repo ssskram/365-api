ba7479d429e7a32870b7fff4e684ecebef48a099
ssskram <paul.marks@pittsburghpa.gov>
2019-06-05 10:29:46 -0400

    confiscatedGuns endpoints

--- models/confiscatedGuns.js
+++ models/confiscatedGuns.js
@@ -1,20 +1,49 @@
+const moment = require("moment");
+const tz = require("moment-timezone");
+
+const dateTransform = date =>
+  moment(date)
+    .tz("America/New_York")
+    .format("MM-DD-YYYY, hh:mm A");
 
 const allUsers = {
   list: "value",
   item: {
-    user: "Email",
+    user: "Email"
   }
 };
 
 const allGuns = {
   list: "value",
   item: {
-    itemID: "ItemID",
-    from: "From",
-    to: "To",
-    pickedUp: "PickedUp",
-    returned: "Returned"
-  }
+    serialNumber: "serialNumber",
+    gunMake: "gunMake",
+    gunModel: "gunModel",
+    gunCaliber: "gunCaliber",
+    dateOfRecovery: "dateOfRecovery",
+    gunType: "gunType",
+    originatingAgencyCaseNumber: "originatingAgencyCaseNumber",
+    linkageAgencyIdentifier: "linkageAgencyIdentifier",
+    linkageAgencyCaseNumber: "linkageAgencyCaseNumber",
+    notifyOriginatingAgency: "notifyOriginatingAgency",
+    miscellaneous: "miscellaneous",
+    name: "name",
+    dateOfBirth: "dateOfBirth",
+    operatorsLicenseNumber: "operatorsLicenseNumber",
+    operatorsLicenseNumber: "operatorsLicenseNumber",
+    licenseState: "licenseState",
+    possessedByProhibitedPerson: "possessedByProhibitedPerson",
+    chargedWithCrime: "chargedWithCrime",
+    permitInLTC: "permitInLTC",
+    createdBy: "createdBy",
+    created: "Created"
+  },
+  operate: [
+    {
+      run: dateTransform,
+      on: "created"
+    }
+  ]
 };
 
 module.exports = {
--- routes/confiscatedGuns.js
+++ routes/confiscatedGuns.js
@@ -7,6 +7,7 @@ const models = require("../models/confiscatedGuns");
 
 global.Headers = fetch.Headers;
 
+// returns all users
 router.get("/allUsers", async (req, res) => {
   fetch(
     "https://cityofpittsburgh.sharepoint.com/sites/Police/ConfiscatedGuns/_api/web/sitegroups(626)/users?$select=Email",
@@ -23,6 +24,43 @@ router.get("/allUsers", async (req, res) => {
       res.status(200).send(dt(data, models.allUsers).transform());
     })
     .catch(err => res.status(500).send(err));
+});
+
+// returns all confiscated  gun  records
+router.get("/allRecords", async (req, res) => {
+  fetch(
+    "https://cityofpittsburgh.sharepoint.com/sites/Police/ConfiscatedGuns/_api/web/lists/GetByTitle('Confiscated Guns')/items",
+    {
+      method: "get",
+      headers: new Headers({
+        Authorization: "Bearer " + (await refreshToken()),
+        Accept: "application/json"
+      })
+    }
+  )
+    .then(res => res.json())
+    .then(data => {
+      res.status(200).send(dt(data, models.allGuns).transform());
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+// create a confiscation record
+router.post("/newRecord", async (req, res) => {
+  await fetch(
+    "https://cityofpittsburgh.sharepoint.com/sites/Police/ConfiscatedGuns/_api/web/lists/GetByTitle('Confiscated Guns')/items",
+    {
+      method: "POST",
+      headers: new Headers({
+        Authorization: "Bearer " + (await refreshToken()),
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }),
+      body: JSON.stringify(req.body)
+    }
+  )
+    .then(() => res.status(200).send())
+    .catch(error => res.status(500).send(error));
 });
 
 module.exports = router;
